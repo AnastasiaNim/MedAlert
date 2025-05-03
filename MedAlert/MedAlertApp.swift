ae8e42122b06da31d5b7618d7fc0770b968c7def
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-05-03 20:10:13 +0300

    Initial Commit

---
+++ MedAlert/MedAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedAlertApp.swift
+//  MedAlert
+//
+//  Created by Anastasia N.  on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MedAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
